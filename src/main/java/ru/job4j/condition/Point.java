56ec5a1626549c63b23cb42bae9a840cf5245073
Любовь <n.lubovj@gmail.com>
2022-03-21 23:32:59 +0300

    Overload Point constructor, add distance3D method in Point class and add tests. Correct Reduce class

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -4,19 +4,34 @@ public class Point {
 
     private final int x;
     private final int y;
+    private final int z;
 
     public Point(int x, int y) {
+        this(x, y, 0);
+    }
+
+    public Point(int x, int y, int z) {
         this.x = x;
         this.y = y;
+        this.z = z;
     }
 
     public double distance(Point that) {
         return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
     }
 
+    public double distance3D(Point that) {
+        return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2) + Math.pow(this.z - that.z, 2));
+    }
+
+    public void info() {
+        System.out.println("Point[" + this.x + ", " + this.y + "]");
+    }
+
     public static void main(String[] args) {
         Point a = new Point(0, 0);
         Point b = new Point(0, 2);
-        System.out.println("the distance is " + a.distance(b) + " metres");    // (:
+        System.out.println("the distance is " + a.distance(b) + " metres");
+        a.info();
     }
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -8,8 +8,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class PointTest {
 
     @Test
-    @DisplayName("Test method distance when x1 = 0, y1 = 0, x2 = 2, y2 =0")
-    void distanceWhenX1Is0Y1Is0X2Is2Y2Is0() {
+    @DisplayName("Test method distance when x1 = 0, y1 = 0, x2 = 2, y2 = 0, then result = 2")
+    void distanceWhenX1Is0Y1Is0X2Is2Y2Is0Then2() {
         Point a = new Point(0, 0);
         Point b = new Point(2, 0);
         double expected = 2;
@@ -19,8 +19,8 @@ class PointTest {
     }
 
     @Test
-    @DisplayName("Test method distance when x1 = 0, y1 = 0, x2 = 0, y2 = 2")
-    void distanceWhenX1Is0Y1Is0X2Is0Y2Is2() {
+    @DisplayName("Test method distance when x1 = 0, y1 = 0, x2 = 0, y2 = 2, then result = 2")
+    void distanceWhenX1Is0Y1Is0X2Is0Y2Is2Then2() {
         Point a = new Point(0, 0);
         Point b = new Point(0, 2);
         double expected = 2;
@@ -30,12 +30,56 @@ class PointTest {
     }
 
     @Test
-    @DisplayName("Test method distance when x1 = 0, y1 = 0, x2 = 0, y2 = 0")
-    void distanceWhenX1Is0Y1Is0X2Is0Y2Is0() {
+    @DisplayName("Test method distance when x1 = 0, y1 = 0, x2 = 0, y2 = 0, then result = 0")
+    void distanceWhenX1Is0Y1Is0X2Is0Y2Is0Then0() {
         Point a = new Point(0, 0);
         Point b = new Point(0, 0);
         double expected = 0;
         double out = a.distance(b);
+        double eps = 0.01;
+        assertEquals(expected, out, eps);
+    }
+
+    @Test
+    @DisplayName("Test method distance3D when x1 = 0, y1 = 0, z1 = 0, x2 = 0, y2 = 0, z2 = 1, then result = 1")
+    void testDistance3DWhenX1Is0Y1Is0Z1Is0X2Is0Y2Is0ZIs1Then1() {
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(0, 0, 1);
+        double expected = 1;
+        double out = a.distance3D(b);
+        double eps = 0.01;
+        assertEquals(expected, out, eps);
+    }
+
+    @Test
+    @DisplayName("Test method distance3D when x1 = 6, y1 = 6, z1 = 6, x2 = 4, y2 = 4, z2 = 4, then result = 3.464")
+    void testDistance3DWhenX1Is6Y1Is6Z1Is6X2Is4Y2Is4ZIs4Then3Point464() {
+        Point a = new Point(6, 6, 6);
+        Point b = new Point(4, 4, 4);
+        double expected = 3.464;
+        double out = a.distance3D(b);
+        double eps = 0.01;
+        assertEquals(expected, out, eps);
+    }
+
+    @Test
+    @DisplayName("Test method distance3D when x1 = 0, y1 = 0, z1 = 0, x2 = 0, y2 = 0, z2 = 0, then result = 0")
+    void testDistance3DWhenX1Is0Y1Is0Z1Is0X2Is0Y2Is0ZIs0Then0() {
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(0, 0, 0);
+        double expected = 0;
+        double out = a.distance3D(b);
+        double eps = 0.01;
+        assertEquals(expected, out, eps);
+    }
+
+    @Test
+    @DisplayName("Test method distance3D when x1 = -1, y1 = -1, z1 = -1, x2 = 4, y2 = 4, z2 = 4, then result = 8.66")
+    void testDistance3DWhenX1IsMinus1Y1IsMinus1Z1IsMinus1X2Is4Y2Is4ZIs4Then8Point66() {
+        Point a = new Point(-1, -1, -1);
+        Point b = new Point(4, 4, 4);
+        double expected = 8.66;
+        double out = a.distance3D(b);
         double eps = 0.01;
         assertEquals(expected, out, eps);
     }
