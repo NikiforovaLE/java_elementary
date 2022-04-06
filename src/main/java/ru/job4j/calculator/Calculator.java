5a35aaf72777c801a287d6d883a0125f5e4d158e
Любовь <n.lubovj@gmail.com>
2022-04-06 23:38:33 +0300

    Change Calculator class

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -2,25 +2,25 @@ package ru.job4j.calculator;
 
 public class Calculator {
 
-    public static void plus (int a, int b) {
+    public static void plus(int a, int b) {
         int result = a + b;
-        System.out.println(String.format("%d + %d = %d", a, b, result));
+        System.out.printf("%d + %d = %d%n", a, b, result);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int one = 1;
         int two = 2;
         int six = 6;
         int four = 4;
         int five = 5;
 
-        Calculator.plus(one,two);
+        Calculator.plus(one, two);
 
-        int sixDivTwo = six/two;
+        int sixDivTwo = six / two;
         int fiveMinusTwo = five - two;
-        int fourTimeTwo = four*two;
+        int fourTimeTwo = four * two;
 
-        System.out.println("6/2 = " +sixDivTwo);
+        System.out.println("6 / 2 = " + sixDivTwo);
         System.out.println("5 - 2 = " + fiveMinusTwo);
         System.out.print("4 * 2 = " + fourTimeTwo);
     }
